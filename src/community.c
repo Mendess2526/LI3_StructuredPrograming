5a059de1911eb4990f6f2e0edf0b45c4e4456614
Mendess2526 <pedro.mendes.26@gmail.com>
2018-03-24 01:24:02 +0000

    Change key alloc to glib implementation
    Add printUsers to print all the users in the hashtable

--- src/community.c
+++ src/community.c
@@ -10,14 +10,10 @@ struct TCD_community{
     SO_USERS users;
 };
 
-long *newId(long val){
-    long *id = (long *) malloc(sizeof(long));
-    *id = val;
+gint64 *newId(long val){
+    gint64 *id = g_new(gint64, 1);
+    *id = so_user_get_id(user);
     return id;
-}
-
-void free_long(void *id){
-    free((long *)id);
 }
 
 static void updateUserPosts(SO_USERS users, POST post){
@@ -38,9 +34,47 @@ static void updateUserPosts(SO_USERS users, POST post){
     so_user_add_post(user,post);
 }
 
+//TODO me no like this
+TAD_community init(){
+    return community_create();
+}
+
+TAD_community community_create(){
+    TAD_community com = (TAD_community) malloc(sizeof(struct TCD_community));
+    com->questions = g_hash_table_new_full(
+            g_int64_hash, g_int64_equal,g_free,question_destroy_generic);
+    com->answers   = g_hash_table_new_full(
+            g_int64_hash, g_int64_equal,g_free,answer_destroy_generic);
+    com->users     = g_hash_table_new_full(
+            g_int64_hash, g_int64_equal,g_free,so_user_destroy_generic);
+    return com;
+}
+
+void community_destroy(TAD_community com){
+    g_hash_table_destroy(com->questions);
+    g_hash_table_destroy(com->answers);
+    g_hash_table_destroy(com->users);
+    free(com);
+}
+
+static SO_USER mergeUserInHashTable(TAD_community com, SO_USER user, gint64 *id){
+    long key = 0;
+    SO_USER value = NULL;
+    gboolean exists = g_hash_table_lookup_extended(
+            com->users,
+            (gconstpointer) &id,
+            (gpointer) &key,
+            (gpointer) &value
+    );
+    if(exists && value != NULL){
+        user = so_user_merge(user,value);
+    }
+    return user;
+}
+
 void community_add_question(TAD_community com, QUESTION question){
     long *id = newId(question_get_id(question));
-    //BEGIN merge post in hash table
+    //BEGIN merge post in hash table TODO abstract
     long key = 0;
     QUESTION newQuestion = NULL;
     gboolean exists = g_hash_table_lookup_extended(
@@ -77,47 +111,13 @@ void community_add_answer(TAD_community com, ANSWER answer){
     question_add_answer(question,answer);
     updateUserPosts(com->users, (POST) answer);
 }
-//TODO me no like this
-TAD_community init(){
-    long *id = newId(4L);
-    printf("print long: %ld\n",*id);
-    free_long(id);
-    return community_create();
-}
-
-TAD_community community_create(){
-    TAD_community com = (TAD_community) malloc(sizeof(struct TCD_community));
-    com->questions = g_hash_table_new_full(
-            g_int64_hash, g_int64_equal,free_long,question_destroy_generic);
-    com->answers   = g_hash_table_new_full(
-            g_int64_hash, g_int64_equal,free_long,answer_destroy_generic);
-    com->users     = g_hash_table_new_full(
-            g_int64_hash, g_int64_equal,free_long,so_user_destroy_generic);
-    return com;
-}
-
-void community_destroy(TAD_community com){
-    g_hash_table_destroy(com->questions);
-    g_hash_table_destroy(com->answers);
-    g_hash_table_destroy(com->users);
-    free(com);
-}
 
 void community_add_user(TAD_community com, SO_USER user){
-    long *id = newId(so_user_get_id(user));
-    //BEGIN merge user in hash table
-    long key = 0;
-    SO_USER value = NULL;
-    gboolean exists = g_hash_table_lookup_extended(
-            com->users,
-            (gconstpointer) &id,
-            (gpointer) &key,
-            (gpointer) &value
-    );
-    if(exists && value != NULL){
-        user = so_user_merge(user,value);
-    }
-    //END merge user in hash table
+    gint64 *id = g_new(gint64, 1);
+    *id = so_user_get_id(user);
+
+    user = mergeUserInHashTable(com, user, id);
+    
     g_hash_table_insert(com->users, (gpointer) &id, user);
 }
 
@@ -148,4 +148,16 @@ void community_add_favorite(TAD_community com, long id){
 
 SO_USER community_get_user(TAD_community com, long id){
     return g_hash_table_lookup(com->users,(gconstpointer) &id);
+}
+
+void printUser(gpointer key, gpointer value, gpointer user_data){
+    long id = so_user_get_id((SO_USER) value);
+    int reputation = so_user_get_reputation((SO_USER) value);
+    xmlChar *name = so_user_get_name((SO_USER) value);
+    xmlChar *bio = so_user_get_bio((SO_USER) value);
+    printf((char *)user_data, *((gint64 *)key), id, reputation, name, bio);
+}
+
+void printUsers(TAD_community com){
+    g_hash_table_foreach(com->users, printUser, "Key{%ld} User{id:%ld, reputation:%ld, name:%ld, bio:%ld\n");
 }
