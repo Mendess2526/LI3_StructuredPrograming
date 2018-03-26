30e4eaa8d5fad4939e692abbbc47599a86364833
Barbara29 <babycardoso98@hotmail.com>
2018-03-26 18:00:02 +0100

    Acabei DateTime

--- include/dateTime.h
+++ include/dateTime.h
@@ -3,4 +3,24 @@
 
 typedef struct _dateTime *DATETIME
 
+DATETIME dateTime_create(int ano,
+                        int mes,
+                        int dia,
+                        int horas,
+                        int minutos,
+                        int segundos,
+                        int milissegundos);
 
+int dateTime_get_ano(DATETIME d);
+
+int dateTime_get_mes(DATETIME d);
+
+int dateTime_get_dia(DATETIME d);
+
+int dateTime_get_horas(DATETIME d);
+
+int dateTime_get_minutos(DATETIME d);
+
+int dateTime_get_segundos(DATETIME d);
+
+int dateTime_get_milissegundos(DATETIME d);
