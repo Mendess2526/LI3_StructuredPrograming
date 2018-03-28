df97ae4203682ad5fd8dd8252d6a19f89adc6397
Mendess2526 <pedro.mendes.26@gmail.com>
2018-03-28 12:39:56 +0100

    Fix calendario header

--- include/calendario.h
+++ include/calendario.h
@@ -7,14 +7,11 @@
 typedef struct _calendario *CALENDARIO;
 
 
-gint timeCompare(gconstpointer a, gconstpointer b);
-
-void getId(gpointer data, gpointer user_data);
-
 CALENDARIO calendario_create();
 
 void calendario_add_post(CALENDARIO cal, POST post );
 
 long *calendario_get_ids(CALENDARIO cal, Date from, Date to);
 
+void calendario_destroy(CALENDARIO cal);
 #endif
