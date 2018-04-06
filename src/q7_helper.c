e180296117ca31089e0c175a2e7be8061676db6f
Mendess2526 <pedro.mendes.26@gmail.com>
2018-04-06 22:34:20 +0100

    Optimize and correct q7_helper

--- src/q7_helper.c
+++ src/q7_helper.c
@@ -5,42 +5,58 @@
 #include <glib.h>
 
 typedef struct collector{
-    int N;
+    int maxSize;
     GSList* list;
 }*COLLECTOR;
 
+void collect(COLLECTOR col, gpointer elem, CCompareFunc cmpFunc){
+    if(col->list == NULL || (*cmpFunc)(col->list->data, elem) < 0)
+        col->list = g_slist_prepend(col->list, elem);
+    else{
+        int i = 0;
+        for(GSList* cur = col->list; cur && i < col->maxSize; cur = cur->next, ++i){
+            if(!cur->next || (*cmpFunc)(cur->next->data, elem) < 0){
+                    cur->next = g_slist_prepend(cur->next, elem);
+                    i = col->maxSize;
+            }
+        }
+    }
+}
+
 int compareScore(gconstpointer a, gconstpointer b){
-    int aS = question_get_score((QUESTION) a);
-    int bS = question_get_score((QUESTION) b);
-    return (aS > bS) - (aS < bS);
+    int aC = question_get_answer_count((QUESTION) a);
+    int bC = question_get_answer_count((QUESTION) b);
+    return aC - bC;
 }
 
-
-void collectFunc(gpointer user_data, gpointer elem){
-    COLLECTOR col = (COLLECTOR) user_data;
-    QUESTION q = (QUESTION) elem;
-    col->list = g_slist_insert_sorted(col->list,q,compareScore);
+void collectFunc(gpointer elem, gpointer user_data){
+    collect((COLLECTOR) user_data, (QUESTION) elem, compareScore);
 }
 
-LONG_list gslist2llist(GSList* list, int N){
-    LONG_list l = create_list(N);
+LONG_list gslist2llist(GSList* list, int maxSize){
+    LONG_list l = create_list(maxSize);
     int i = 0;
-    for(QUESTION question = list->data; list && i<N; list = list->next){
-        set_list(l, i++, question_get_id(question));
+    for(; list && i<maxSize; list = list->next){
+        set_list(l, i++, question_get_id(list->data));
     }
     return l;
 }
 
 LONG_list most_answered_questions_helper(TAD_community com,
-                                        int N,
+                                        int maxSize,
                                         Date begin,
                                         Date end){
 
     COLLECTOR col = (COLLECTOR) malloc(sizeof(struct collector));
     col->list = NULL;
-    col->N = N;
+    col->maxSize = maxSize;
 
     community_iterate_questions(com, begin, end, col, collectFunc);
 
-    return gslist2llist(col->list, N);
+    LONG_list r = gslist2llist(col->list, maxSize);
+
+    g_slist_free(col->list);
+    g_free(col);
+
+    return r;
 }
