73a4c331f04f37550f4ecf5bd7f4dc2be1a74540
Barbara29 <babycardoso98@hotmail.com>
2018-04-13 00:26:13 +0100

    Implementa query 10

--- src/interface.c
+++ src/interface.c
@@ -101,14 +101,23 @@ LONG_list both_participated(TAD_community com, long id1, long id2, int N){
 
 // query 10
 long better_answer(TAD_community com, long id){
-/*    QUESTION question = community_get_question(com,id);
+    QUESTION question = community_get_question(com,id);
     ANSWERS answers = question_get_answers(question);
-    int score = answer_get_score(answer);
-    long idUser = answer_get_owner_id(answer);
-    int rep = so_user_get_reputation(user)
-    int nrCom = answer_get_comment_count(answer);
-    for(aswers;)
-    return 0L;*/
+    int melhorp = 0;
+    int idmelhor = 0;
+    for(ANSWERS cur = answers;cur != NULL;cur = cur->next){
+        int score = answer_get_score(cur->data);
+        long idUser = answer_get_owner_id(cur->data);
+        SO_USER user = community_get_user(com,idUser);
+        int rep = so_user_get_reputation(user);
+        int nrCom = answer_get_comment_count(cur->data);
+        int pontuacao = (score*0.65)+(rep*0.25)+(nrCom*0.1);
+        if(pontuacao>melhorp){
+            melhorp = pontuacao;
+            idmelhor = idUser;
+        } 
+    }
+    return idmelhor;
 }
 
 // query 11
