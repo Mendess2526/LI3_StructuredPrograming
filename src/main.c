cddcaa68ad05122aa499636438b9c9dcc39d2a16
Mendess2526 <pedro.mendes.26@gmail.com>
2018-04-12 17:36:59 +0100

    Implements query 4

--- src/main.c
+++ src/main.c
@@ -55,7 +55,7 @@ int main(int argc, const char **argv){
     printAndDestroyLongPair(total_posts(com, beginingOfTime, theHeatDeath));
 
     printf("%sQUERY  4:%s Questions with tag\n",BLUE, RESET);
-    printAndDestroyLongList(questions_with_tag(com, "battery", beginingOfTime, theHeatDeath), 0);
+    printAndDestroyLongList(questions_with_tag(com, "sms", beginingOfTime, theHeatDeath), 3);
 
     printf("%sQUERY  5:%s Get user info\n",BLUE, RESET);
     printAndDestroyUser(get_user_info(com, 5));
