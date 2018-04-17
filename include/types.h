062c744c4b493b10a93b0e334575799991fbe6b4
Barbara29 <babycardoso98@hotmail.com>
2018-04-17 14:12:33 +0100

    Documentaçao

--- include/types.h
+++ include/types.h
@@ -3,32 +3,74 @@
 
 #include <glib.h>
 
-typedef struct TCD_community *TAD_community;
+ /**
+ * .
+ * .
+ */
+typedef struct TCD_community * TAD_community;
 
+ /**
+ * .
+ * .
+ */
+typedef struct _post * POST;
 
-typedef struct _post *POST;
+ /**
+ * .
+ * .
+ */
+typedef struct _question * QUESTION;
 
+ /**
+ * .
+ * .
+ */
+typedef GSList * ANSWERS;
 
-typedef struct _question *QUESTION;
+ /**
+ * .
+ * .
+ */
+typedef struct _answer * ANSWER;
 
+ /**
+ * .
+ * .
+ */
+typedef struct _so_user * SO_USER;
 
-typedef struct _answer *ANSWER;
+ /**
+ * .
+ * .
+ */
+typedef struct _dateTime * DATETIME;
 
+ /**
+ * .
+ * .
+ */
+typedef struct _calendario * CALENDARIO;
 
-typedef struct _so_user *SO_USER;
-
-
-typedef struct _dateTime *DATETIME;
-
-
-typedef struct _calendario *CALENDARIO;
-
+ /**
+ * .
+ * .
+ */
 typedef int (*CFunc)(void* elem, void* user_data);
 
+ /**
+ * .
+ * .
+ */
 typedef GDestroyNotify CFreeFunc;
-
+ /**
+ * .
+ * .
+ */
 typedef GCompareFunc CCompareFunc;
-
+ /**
+ * .
+ * .
+ */
 typedef void (*CPrintFunction)(void*);
 
 #endif /*__TYPES_H__*/
