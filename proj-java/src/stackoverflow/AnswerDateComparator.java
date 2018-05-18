12484f38b0591d982b12efc4ab09ec6ee059be13
Barbara29 <babycardoso98@hotmail.com>
2018-05-18 17:24:14 +0100

    Muda AnswerComparator para PostComparator

--- proj-java/src/stackoverflow/AnswerDateComparator.java
+++
@@ -1,9 +0,0 @@
-package stackoverflow;
-
-import java.util.Comparator;
-
-public class AnswerDateComparator implements Comparator<Answer> {
-
-    public int compare(Answer answer1, Answer answer2){
-        return answer1.getDate().compareTo(answer2.getDate()); }
-}
---
+++ proj-java/src/stackoverflow/PostDateComparator.java
@@ -0,0 +1,9 @@
+package stackoverflow;
+
+import java.util.Comparator;
+
+public class PostDateComparator implements Comparator<Post> {
+
+    public int compare(Post post1, Post post2){
+        return post1.getDate().compareTo(post2.getDate()); }
+}
