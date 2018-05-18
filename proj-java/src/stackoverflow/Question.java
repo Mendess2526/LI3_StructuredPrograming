9b2fde6202f86df6e3c109aa7e8baf74412ebcad
Barbara29 <babycardoso98@hotmail.com>
2018-05-18 19:03:11 +0100

    Question

--- proj-java/src/stackoverflow/Question.java
+++ proj-java/src/stackoverflow/Question.java
@@ -1,7 +1,10 @@
 package stackoverflow;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Objects;
+import java.util.StringTokenizer;
 
 public class Question extends Post{
     private int answerCount;
@@ -9,12 +12,12 @@ public class Question extends Post{
     private String tags;
     private LinkedList<Answer> answers;
 
-    public Question(long id, LocalDateTime date, int score, long ownerId, int answerCount, String ownerName, String title, String tags, LinkedList<Answer> answers){
+    public Question(long id, LocalDateTime date, int score, long ownerId, int answerCount, String ownerName, String title, String tags){
         super(score, id, ownerId, date, ownerName);
         this.answerCount = answerCount;
         this.title = title;
         this.tags = tags;
-        this.answers = answers;
+        this.answers = new LinkedList<>();
     }
 
     public int getAnswerCount(){
@@ -25,14 +28,29 @@ public class Question extends Post{
         return this.title;
     }
 
-    //TODO array de strings
-    public String getTags(){
-        return this.tags;
+    public String[] getTags(){
+        String[] tags = new String[5];
+        StringTokenizer st = new StringTokenizer(this.tags.substring(1), "><");
+        int i = 0;
+        while (st.hasMoreTokens()){
+            String s = st.nextToken();
+            tags[i++] = s;
+        }
+        return tags;
     }
 
     //TODO
     public LinkedList<Answer> getAnswers(){
         return this.answers;
+    }
+
+    public void addAnswer(Answer answer){
+        answer.setParentPtr(this);
+        answers.addFirst(answer);
+    }
+
+    public boolean hasTag(String tag){
+        return this.tags.contains(tag);
     }
 
     @Override
@@ -43,6 +61,17 @@ public class Question extends Post{
                 ", tags='" + tags + '\'' +
                 ", answers=" + answers +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question = (Question) o;
+        return answerCount == question.answerCount &&
+                Objects.equals(title, question.title) &&
+                Objects.equals(tags, question.tags) &&
+                Objects.equals(answers, question.answers);
     }
 
 }
--- proj-java/src/stackoverflow/QuestionAnswerCountComparator.java
+++ proj-java/src/stackoverflow/QuestionAnswerCountComparator.java
@@ -1,4 +1,11 @@
 package stackoverflow;
 
-public class QuestionAnswerCountComparator {
+import java.util.Comparator;
+
+public class QuestionAnswerCountComparator implements Comparator<Question>{
+
+    @Override
+    public int compare(Question question1, Question question2){
+        return Integer.compare(question1.getAnswerCount(), question2.getAnswerCount());
+    }
 }
