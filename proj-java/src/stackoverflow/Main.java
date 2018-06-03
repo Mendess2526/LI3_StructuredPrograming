c9ac322474fb87cf2550c6e4ed615f7034f7bd94
Mendess2526 <pedro.mendes.26@gmail.com>
2018-06-03 13:15:04 +0100

    Change main

--- proj-java/src/stackoverflow/Main.java
+++ proj-java/src/stackoverflow/Main.java
@@ -3,11 +3,13 @@ package stackoverflow;
 public class Main {
 
     public static void main(String[] args) {
-        Parser.parse(new Community(), makeDumpPath(args.length > 2 ? args[1] : null));
+        Community com = new Community();
+        Parser.parse(com, makeDumpPath(args.length > 2 ? args[1] : null));
+
     }
 
     private static String makeDumpPath(String folder){
         if(folder == null) folder = "askubuntu";
-        return System.getProperty("user.home") + "/dump_exemplo/" + folder + "/";
+        return "../dump_exemplo/" + folder + "/";
     }
 }
