eddb199ceadda741a267165db8519935f7b8c440
Barbara29 <babycardoso98@hotmail.com>
2018-06-12 00:05:25 +0100

    Documenta question

--- proj-java/src/main/java/engine/Question.java
+++ proj-java/src/main/java/engine/Question.java
@@ -14,6 +14,17 @@ public class Question extends Post{
     private LocalDate cachedTo;
     private int answerCountBetweenDates;
 
+    /**
+     * Cria uma questão.
+     * @param id O id da questão.
+     * @param date A data da questão.
+     * @param score O score da questão.
+     * @param ownerId O id do autor da questão.
+     * @param answerCount O número de respostas da questão.
+     * @param ownerName O nome do autor da questão.
+     * @param title O título da questão.
+     * @param tags As tags da questão.
+     */
     public Question(long id, LocalDateTime date, int score, long ownerId, int answerCount, String ownerName, String title, String tags){
         super(score, id, ownerId, date, ownerName);
         this.answerCount = answerCount;
@@ -22,14 +33,26 @@ public class Question extends Post{
         this.answers = new LinkedList<>();
     }
 
+    /**
+     * Retorna o número de respostas da questão.
+     * @return O número de respostas da questão.
+     */
     public int getAnswerCount(){
         return this.answerCount;
     }
 
+    /**
+     * Retorna o título da questão.
+     * @return O título da questão.
+     */
     public String getTitle(){
         return this.title;
     }
 
+    /**
+     * Retorna as tags da questão.
+     * @return As tags da questão.
+     */
     public String[] getTags(){
         String[] tags = new String[5];
         StringTokenizer st = new StringTokenizer(this.tags.substring(1), "><");
@@ -41,25 +64,49 @@ public class Question extends Post{
         return tags;
     }
 
+    /**
+     * Retorna a lista de respostas da questão.
+     * @return Retorna a lista de respostas da questão.
+     */
     public List<Answer> getAnswers(){
         return new ArrayList<>(this.answers);
     }
 
+    /**
+     * Adiciona uma resposta à lista de respostas.
+     * @param answer A resposta a adicionar.
+     */
     void addAnswer(Answer answer){
         answer.setParentPtr(this);
         answers.addFirst(answer);
     }
 
+    /**
+     * Verifica se uma questão tem uma tag.
+     * @param tag Uma tag.
+     * @return true se a questão tem a tag, ou false, caso contrário.
+     */
     public boolean hasTag(String tag){
         return this.tags.contains(tag);
     }
 
+    /**
+     * Procura um user nas respostas de uma questão.
+     * @param id O id do user à procura.
+     * @return Uma questão.
+     */
     public Question searchUserInThread(long id){
         if(this.getOwnerId() == id) return this;
         for(Answer a: this.answers) if(a.getOwnerId() == id) return this;
         return null;
     }
 
+    /**
+     * Retorna o número de respostas dentro de um intervalo de tempo.
+     * @param from A data de início.
+     * @param to A data do fim.
+     * @return O número de respostas dentro de um intervalo de tempo.
+     */
     public int getAnswerCountBetweenDate(LocalDate from, LocalDate to){
         if(from.equals(this.cachedFrom) && to.equals(this.cachedTo))
             return this.answerCountBetweenDates;
