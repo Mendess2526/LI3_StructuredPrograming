76c79b47f5ca58065eef64dc1d7a29b99a377ce0
Barbara29 <babycardoso98@hotmail.com>
2018-06-12 00:23:47 +0100

    Documenta post

--- proj-java/src/main/java/engine/Post.java
+++ proj-java/src/main/java/engine/Post.java
@@ -14,7 +14,14 @@ public abstract class Post implements Chronological {
     private final long ownerId;
     private final String ownerName;
 
-
+    /**
+     * Cria um post.
+     * @param score O score do post.
+     * @param id O id do post.
+     * @param ownerId O id do autor do post.
+     * @param date A data do post.
+     * @param ownerName O nome do autor do post.
+     */
     Post(int score, long id, long ownerId, LocalDateTime date, String ownerName){
         this.id = id;
         this.score = score;
@@ -23,23 +30,42 @@ public abstract class Post implements Chronological {
         this.ownerName = ownerName;
     }
 
+    /**
+     * Retorna o id do post.
+     * @return O id do post.
+     */
     public long getId(){
         return this.id;
     }
 
-    @Override
+    /**
+     * Retorna a data do post.
+     * @return A data do post.
+     */
     public LocalDateTime getDate(){
         return this.date;
     }
 
+    /**
+     * Retorna o score do post.
+     * @return O id do post.
+     */
     public int getScore(){
         return this.score;
     }
 
+    /**
+     * Retorna o id do autor do post.
+     * @return O id do autor do post.
+     */
     public long getOwnerId(){
         return this.ownerId;
     }
 
+    /**
+     * Retorna o nome do autor do post.
+     * @return O nome do autor do post.
+     */
     public String getOwnerName(){
         return this.ownerName;
     }
@@ -55,8 +81,15 @@ public abstract class Post implements Chronological {
                 '}';
     }
 
+    //TODO
     public abstract Question searchUserInThread(long id);
 
+    /**
+     * Verifica se o post está dentro de um intervalo de tempo.
+     * @param from A data de início.
+     * @param to A data do fim.
+     * @return true se estiver no intervalo de tempo, ou false, caso contrário.
+     */
     public boolean isBetweenDates(LocalDate from, LocalDate to){
         return this.date.isAfter(from.atStartOfDay()) && this.date.isBefore(to.atTime(LocalTime.MAX));
     }
