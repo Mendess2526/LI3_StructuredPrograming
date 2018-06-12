84eb123fa1415db6d5b495e121a434ecf5efdfe6
Barbara29 <babycardoso98@hotmail.com>
2018-06-12 11:11:13 +0100

    Documenta hour

--- proj-java/src/main/java/engine/calendario/Hour.java
+++ proj-java/src/main/java/engine/calendario/Hour.java
@@ -7,22 +7,44 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.function.Predicate;
 
+/**
+ * Classe que define uma hora.
+ * @param <T> Tipo dos elemnetos guardados.
+ */
 class Hour<T extends Chronological> {
+    /** Lista dos elementos da hora. */
     private final SortedLinkedList<T> elems;
 
+    /**
+     * Cria um hora.
+     */
     Hour(){
         this.elems = new SortedLinkedList<>(Comparator.comparing(Chronological::getDate));
     }
 
+    /**
+     * Adiciona um elemento a uma hora.
+     * @param c Elemento a adicionar.
+     */
     void addElem(T c){
         this.elems.addLast(c);
     }
 
+    /**
+     * Itera sobre os elementos de uma hora por ordem cronológica.
+     * @param predicate Função a aplicar a todos os elementos.
+     * @return {@code true} se a iteração deve continuar, {@code false} caso contrário.
+     */
     boolean iterateForward(Predicate<T> predicate){
         for(T elem : this.elems) if(!predicate.test(elem)) return false;
         return true;
     }
 
+    /**
+     * Itera sobre os elementos de uma hora por ordem cronológica inversa.
+     * @param predicate Função a aplicar a todos os elementos.
+     * @return {@code true} se a iteração deve continuar, {@code false} caso contrário.
+     */
     boolean iterateBackwards(Predicate<T> predicate){
         boolean r = true;
         Iterator<T> it = this.elems.descendingIterator();
@@ -30,6 +52,10 @@ class Hour<T extends Chronological> {
         return r;
     }
 
+    /**
+     * Conta o número de elementos que a hora tem.
+     * @return O número de elementos que a hora tem.
+     */
     int getCount(){
         return this.elems.size();
     }
