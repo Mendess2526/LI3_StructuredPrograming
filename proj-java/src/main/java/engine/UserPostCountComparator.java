19b683a5c2c9b078c5779a8b5b5a8ce056af528e
Barbara29 <babycardoso98@hotmail.com>
2018-06-12 15:18:53 +0100

    Documenta o comparador do numero dos posts

--- proj-java/src/main/java/engine/UserPostCountComparator.java
+++ proj-java/src/main/java/engine/UserPostCountComparator.java
@@ -2,8 +2,14 @@ package engine;
 
 import java.util.Comparator;
 
+/**
+ * Comparador do número de posts dos users.
+ */
 class UserPostCountComparator implements Comparator<User>{
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int compare(User user1, User user2){
         return Integer.compare(user1.getNrPosts(),user2.getNrPosts());
