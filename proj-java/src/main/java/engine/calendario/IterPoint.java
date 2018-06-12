6b8ee0d6080a4492c9d72a379f87d8c8f5e15c1d
Mendess2526 <pedro.mendes.26@gmail.com>
2018-06-12 11:01:50 +0100

    Documents IterPoint

--- proj-java/src/main/java/engine/calendario/IterPoint.java
+++ proj-java/src/main/java/engine/calendario/IterPoint.java
@@ -1,11 +1,32 @@
 package engine.calendario;
 
+/**
+ * Enum que define o ponto de iteração do calendário.
+ */
 enum IterPoint {
-    IS_START(true,false), IS_END(false,true), IS_BOTH(true,true), IS_NEITHER(false,false);
+    /** Está no início da iteração. */
+    IS_START(true,false),
+    /** Está no fim da iteração. */
+    IS_END(false,true),
+    /** Está no início e no fim da iteração. */
+    IS_BOTH(true,true),
+    /** Não está no início nem no fim. */
+    IS_NEITHER(false,false);
 
+    /** {@code true} se estiver pelo menos no início, {@code false} caso contrário. */
     final boolean isStart;
+    /** {@code true} se estiver pelo menos no fim, {@code false} caso contrário. */
     final boolean isEnd;
 
+    /**
+     * Cria uma instância do enum a partir de 2 booleans.
+     * @param isStart Se está no início da iteração.
+     * @param isEnd Se está no fim da iteração.
+     * @return {@code IS_START} se for passado {@code (true, false)}
+     *         {@code IS_END} se for passado {@code (false,true)}
+     *         {@code IS_BOTH} se for passado {@code (true, true)}
+     *         {@code IS_NEITHER} se for passado {@code (false, false)}.
+     */
     static IterPoint make(boolean isStart, boolean isEnd){
         if(isStart)
             if(isEnd) return IterPoint.IS_BOTH;
@@ -15,6 +36,11 @@ enum IterPoint {
             else return IterPoint.IS_NEITHER;
     }
 
+    /**
+     * Construtor do enum.
+     * @param isStart Se está no início da iteração.
+     * @param isEnd Se está no fim da iteração.
+     */
     IterPoint(boolean isStart, boolean isEnd){
         this.isStart = isStart;
         this.isEnd = isEnd;
