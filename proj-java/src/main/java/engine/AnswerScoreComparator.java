76a488baba964fe908d3244af1d7618adb1fd5cb
Barbara29 <babycardoso98@hotmail.com>
2018-06-12 12:57:32 +0100

    Documenta o comparador de respostas

--- proj-java/src/main/java/engine/AnswerScoreComparator.java
+++ proj-java/src/main/java/engine/AnswerScoreComparator.java
@@ -2,8 +2,15 @@ package engine;
 
 import java.util.Comparator;
 
+/**
+ * Comparador do score das respostas.
+ */
 class AnswerScoreComparator implements Comparator<Answer> {
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int compare(Answer answer1, Answer answer2){
         return Integer.compare(answer1.getScore(),answer2.getScore());
